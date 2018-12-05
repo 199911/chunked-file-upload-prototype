067dcf8dbb1c24605e5f72ebdb28e19dde4f8e52
KU Lok Sun <sundaykuloksun@gmail.com>
2018-12-05 15:15:54 +0800

    feat: add api to get uploaded files

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Upload file. */
+router
+  .post('/files',
+    function(req, res, next) {
+      req.rawBody = '';
+      req.setEncoding('utf8');
+      req.on('data', function(chunk) {
+        req.rawBody += chunk;
+      });
+      req.on('end', function() {
+        next();
+      });
+    },
+    function(req, res, next) {
+      console.log(req.rawBody);
+      res.send('OK');
+    }
+  );
+
 module.exports = router;
