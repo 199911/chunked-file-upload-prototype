42c84fcd668d989e58b2711f6568b46b4b09ddfe
KU Lok Sun <sundaykuloksun@gmail.com>
2018-12-05 17:32:52 +0800

    feat: add example on front-end chunk file upload

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -6,10 +6,38 @@ jsUpload.onsubmit = function() {
     formData.append('files', input.files[i]);
   }
 
-  fetch('files', {
+  fetch('/files', {
     method: 'POST',
     body: formData
   })
   .then(response => console.log('Success:', response))
   .catch(error => console.error('Error:', error));
+}
+
+chunkUpload.onsubmit = function() {
+  const input = document.querySelector("#chunkUpload input[type='file']");
+  [ ...input.files ].forEach((file) => {
+    const size = file.size;
+    const sliceSize = 8;
+
+    const parts = [];
+    let start = 0;
+    let end = sliceSize;
+    while(start < size) {
+      parts.push(file.slice(start, end));
+      start = end;
+      end += sliceSize;
+    }
+
+    parts.forEach((part, index) => {
+      const formData = new FormData();
+      formData.append(`file${index}`, part);
+      fetch('/files', {
+        method: 'POST',
+        body: formData
+      })
+        .then(response => console.log('Success:', response))
+        .catch(error => console.error('Error:', error));
+    });
+  });
 }
